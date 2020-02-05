7cd83b397d4b2b60db4322719038e1c087e6af93
Ivaylo Badinov <venimus@gmail.com>
2020-02-05 15:55:39 +0200

    Base path of favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,13 +5,13 @@ module.exports = {
         ['link', {
             rel: 'icon',
             type: 'image/png',
-            href: basePath + 'img/favicon-16x16.png',
+            href: '/img/favicon-16x16.png',
             sizes: '16x16'
         }],
         ['link', {
             rel: 'icon',
             type: 'image/png',
-            href: basePath + 'img/favicon-32x32.png',
+            href: '/img/favicon-32x32.png',
             sizes: '32x32'
         }],
         ['link', {rel: 'canonical', href: 'https://aeternity.com' + basePath}],
